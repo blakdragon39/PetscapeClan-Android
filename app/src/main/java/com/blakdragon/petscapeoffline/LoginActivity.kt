ace4095bbf7dfb7b388c44feb72b3b964551aa99
Jenna Link <jenna.hartley@pushinteractions.com>
2021-05-09 20:20:44 -0600

    Initial commit, with some styles

---
+++ app/src/main/java/com/blakdragon/petscapeoffline/LoginActivity.kt
@@ -0,0 +1,11 @@
+package com.blakdragon.petscapeoffline
+
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginActivity : AppCompatActivity() {
+
+    private val auth = FirebaseAuth.getInstance()
+
+
+}
