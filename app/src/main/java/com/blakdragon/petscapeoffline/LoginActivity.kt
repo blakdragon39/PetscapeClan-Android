b52e385fb98e4c993a184120afd49a99f6950310
Jenna Link <jenna.hartley@pushinteractions.com>
2021-05-10 19:06:05 -0600

    Added google sign in button

--- app/src/main/java/com/blakdragon/petscapeoffline/LoginActivity.kt
+++ app/src/main/java/com/blakdragon/petscapeoffline/LoginActivity.kt
@@ -1,14 +1,21 @@
 package com.blakdragon.petscapeoffline
 
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewModelScope
 import com.blakdragon.petscapeoffline.databinding.ActivityLoginBinding
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import timber.log.Timber
 
 class LoginActivity : AppCompatActivity() {
 
@@ -17,10 +24,33 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private val viewModel: LoginViewModel by viewModels()
 
+    private val googleSignInResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (result.resultCode == RESULT_OK && result.data != null) {
+            handleGoogleSignInResult(result.data!!)
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_login)
         binding.viewModel = viewModel
+
+        binding.bGoogleSignIn.setOnClickListener { googleSignIn() }
+    }
+
+    private fun googleSignIn() {
+        val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN).build()
+        val client = GoogleSignIn.getClient(this, options)
+        googleSignInResult.launch(client.signInIntent)
+    }
+
+    private fun handleGoogleSignInResult(data: Intent) = lifecycleScope.launch {
+        try {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).await()
+            Timber.tag("OFFLINE").i(account.displayName)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
     }
 }
 
--- app/src/main/java/com/blakdragon/petscapeoffline/PetscapeApplication.kt
+++ app/src/main/java/com/blakdragon/petscapeoffline/PetscapeApplication.kt
@@ -2,11 +2,16 @@ package com.blakdragon.petscapeoffline
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import timber.log.Timber
 
 class PetscapeApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
 
         FirebaseApp.initializeApp(this)
     }
