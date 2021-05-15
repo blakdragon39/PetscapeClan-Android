f103e3de1a66e63a1a9efadb9af9d23fe08c41d4
Jenna Link <jenna.hartley@pushinteractions.com>
2021-05-15 15:56:33 -0600

    Fixed back press

--- app/src/main/java/com/blakdragon/petscapeoffline/activities/LoginActivity.kt
+++ app/src/main/java/com/blakdragon/petscapeoffline/activities/LoginActivity.kt
@@ -11,12 +11,7 @@ import androidx.lifecycle.*
 import androidx.transition.TransitionManager
 import com.blakdragon.petscapeoffline.R
 import com.blakdragon.petscapeoffline.databinding.ActivityLoginBinding
-import com.blakdragon.petscapeoffline.models.NetworkResult
 import com.blakdragon.petscapeoffline.models.User
-import com.blakdragon.petscapeoffline.network.NetworkInstance
-import com.blakdragon.petscapeoffline.network.requests.GoogleLoginRequest
-import com.blakdragon.petscapeoffline.network.requests.LoginRequest
-import com.blakdragon.petscapeoffline.network.requests.RegisterRequest
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.auth.api.signin.GoogleSignInStatusCodes
@@ -59,8 +54,11 @@ class LoginActivity : BaseActivity() {
     }
 
     override fun onBackPressed() {
-        super.onBackPressed()
-        viewModel.registering.value = false
+        if (viewModel.registering.value == true) {
+            viewModel.registering.value = false
+        } else {
+            super.onBackPressed()
+        }
     }
 
     private fun googleSignIn() {
@@ -84,7 +82,6 @@ class LoginActivity : BaseActivity() {
     }
 
     private fun transitionRegister(registering: Boolean) {
-        binding.bLogin.isVisible = !registering
         binding.etDisplayName.isVisible = registering
 
         val cs = ConstraintSet().apply {
@@ -106,97 +103,5 @@ class LoginActivity : BaseActivity() {
     private fun handleLogin(user: User) {
         Timber.tag("USER").i(user.displayName)
         Timber.tag("USER").i(user.token)
-    }
-}
-
-class LoginViewModel : ViewModel() {
-
-    val email = MutableLiveData("")
-    val password = MutableLiveData("")
-    val displayName = MutableLiveData("")
-
-    val registering = MutableLiveData(false)
-    val busy = MutableLiveData(false)
-
-    val loginEnabled = MediatorLiveData<Boolean>()
-    val registerEnabled = MediatorLiveData<Boolean>()
-
-    val loginResult = MutableLiveData<NetworkResult<User>>()
-
-    //todo loading state
-
-    init {
-        listOf(email, password).forEach {
-            loginEnabled.addSource(it) { checkLoginEnabled() }
-        }
-
-        listOf(registering, email, password, displayName).forEach {
-            registerEnabled.addSource(it) { checkRegisterEnabled() }
-        }
-    }
-
-    fun login() = viewModelScope.launch {
-        busy.value = true
-
-        try {
-            val response = NetworkInstance.API.login(LoginRequest(email = email.value!!, password = password.value!!))
-            loginResult.value = NetworkResult(data = response)
-        } catch (e: Exception) {
-            loginResult.value = NetworkResult(exception = e)
-        }
-
-        busy.value = false
-    }
-
-    fun googleLogin(idToken: String) = viewModelScope.launch {
-        busy.value = true
-
-        try {
-            val response = NetworkInstance.API.googleLogin(GoogleLoginRequest(idToken))
-            loginResult.value = NetworkResult(data = response)
-        } catch (e: Exception) {
-            loginResult.value = NetworkResult(exception = e)
-        }
-
-        busy.value = false
-    }
-
-    fun checkRegistering() {
-        if (registering.value == true) {
-            register()
-        } else {
-            registering.value = true
-            checkRegisterEnabled()
-        }
-    }
-
-    private fun register() = viewModelScope.launch {
-        busy.value = true
-
-        try {
-            val response = NetworkInstance.API.register(RegisterRequest(
-                email = email.value!!,
-                password = password.value!!,
-                displayName = displayName.value!!
-            ))
-            loginResult.value = NetworkResult(data = response)
-        } catch (e: Exception) {
-            loginResult.value = NetworkResult(exception = e)
-        }
-
-        busy.value = false
-    }
-
-    private fun checkLoginEnabled() {
-        loginEnabled.value = email.value.isNullOrEmpty().not() && password.value.isNullOrEmpty().not()
-    }
-
-    private fun checkRegisterEnabled() {
-        registerEnabled.value = registering.value == false ||
-                (
-                    email.value.isNullOrEmpty().not() &&
-                    password.value.isNullOrEmpty().not() &&
-                    displayName.value.isNullOrEmpty().not()
-                )
     }
 }
---
+++ app/src/main/java/com/blakdragon/petscapeoffline/activities/LoginViewModel.kt
@@ -0,0 +1,105 @@
+package com.blakdragon.petscapeoffline.activities
+
+import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.blakdragon.petscapeoffline.models.NetworkResult
+import com.blakdragon.petscapeoffline.models.User
+import com.blakdragon.petscapeoffline.network.NetworkInstance
+import com.blakdragon.petscapeoffline.network.requests.GoogleLoginRequest
+import com.blakdragon.petscapeoffline.network.requests.LoginRequest
+import com.blakdragon.petscapeoffline.network.requests.RegisterRequest
+import kotlinx.coroutines.launch
+
+class LoginViewModel : ViewModel() {
+
+    val email = MutableLiveData("")
+    val password = MutableLiveData("")
+    val displayName = MutableLiveData("")
+
+    val registering = MutableLiveData(false)
+    val busy = MutableLiveData(false)
+
+    val loginEnabled = MediatorLiveData<Boolean>()
+    val registerEnabled = MediatorLiveData<Boolean>()
+
+    val loginResult = MutableLiveData<NetworkResult<User>>()
+
+    init {
+        listOf(email, password).forEach {
+            loginEnabled.addSource(it) { checkLoginEnabled() }
+        }
+
+        listOf(registering, email, password, displayName).forEach {
+            registerEnabled.addSource(it) { checkRegisterEnabled() }
+        }
+    }
+
+    fun login() = viewModelScope.launch {
+        busy.value = true
+
+        try {
+            val response = NetworkInstance.API.login(LoginRequest(email = email.value!!, password = password.value!!))
+            loginResult.value = NetworkResult(data = response)
+        } catch (e: Exception) {
+            loginResult.value = NetworkResult(exception = e)
+        }
+
+        busy.value = false
+    }
+
+    fun googleLogin(idToken: String) = viewModelScope.launch {
+        busy.value = true
+
+        try {
+            val response = NetworkInstance.API.googleLogin(GoogleLoginRequest(idToken))
+            loginResult.value = NetworkResult(data = response)
+        } catch (e: Exception) {
+            loginResult.value = NetworkResult(exception = e)
+        }
+
+        busy.value = false
+    }
+
+    fun checkRegistering() {
+        if (registering.value == true) {
+            register()
+        } else {
+            registering.value = true
+            checkRegisterEnabled()
+        }
+    }
+
+    private fun register() = viewModelScope.launch {
+        busy.value = true
+
+        try {
+            val response = NetworkInstance.API.register(
+                RegisterRequest(
+                email = email.value!!,
+                password = password.value!!,
+                displayName = displayName.value!!
+            )
+            )
+            loginResult.value = NetworkResult(data = response)
+        } catch (e: Exception) {
+            loginResult.value = NetworkResult(exception = e)
+        }
+
+        busy.value = false
+    }
+
+    private fun checkLoginEnabled() {
+        loginEnabled.value = email.value.isNullOrEmpty().not() && password.value.isNullOrEmpty().not()
+    }
+
+    private fun checkRegisterEnabled() {
+        registerEnabled.value = registering.value == false ||
+                (
+                        email.value.isNullOrEmpty().not() &&
+                                password.value.isNullOrEmpty().not() &&
+                                displayName.value.isNullOrEmpty().not()
+                        )
+    }
+}
